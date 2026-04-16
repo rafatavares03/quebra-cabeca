7051acf962441ecc7145edc7a2c5ac12136e288e
Rafael Tavares <rafatavares03@hotmail.com>
2026-04-16 14:34:19 -0300

    refatorando funcoes

--- js/script.js
+++ js/script.js
@@ -40,33 +40,34 @@ async function getImage(width, height) {
   return response.url;
 }
 
-function ativarCarregando() {
+function ativarCarregamento() {
   const loaders = document.querySelectorAll(".loader");
   loaders.forEach((loader) => {
     loader.style.display = 'block';
   })
 }
 
-function desativarCarregando() {
+function desativarCarregamento() {
   const loaders = document.querySelectorAll(".loader");
   loaders.forEach((loader) => {
     loader.style.display = 'none';
   })
 }
 
-
-const baralho = document.getElementById("baralho");
-const tabuleiro = document.getElementById("tabuleiro");
-
-document.addEventListener("DOMContentLoaded", async () => {
-  ativarCarregando();
+async function montarBaralho() {
+  ativarCarregamento();
   let imageURL = await getImage(tabuleiro.offsetWidth, tabuleiro.offsetHeight);
   recortarImagem(imageURL, 5, 4).then((blocos) => {
-    desativarCarregando();
+    desativarCarregamento();
     while(blocos.length > 0) {
       let bloco = Math.floor(Math.random() * blocos.length);
       baralho.appendChild(blocos[bloco]);
       blocos.splice(bloco, 1);
     }
   });
-});
+}
+
+const baralho = document.getElementById("baralho");
+const tabuleiro = document.getElementById("tabuleiro");
+
+document.addEventListener("DOMContentLoaded", montarBaralho());
